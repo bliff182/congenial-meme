ea992e454ce60848e6152c8f51f9c62b3f36d859
bliff182 <michael.bither@gmail.com>
2019-10-26 12:19:11 -0400

    fixed dropdown menu issue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,16 +4,33 @@ $(document).ready(function () {
         $('#myInput').trigger('focus')
     }) */
 
-    $("#team-submit").on("click", function () {
-        var team = $("#team-selection").val().trim().split(/\s+/).join('-');
-        console.log(team);
+    // var team;
+    var teamAbbr;
+    var team;
+    var teamDashed;
 
-        var newsUrl = "https://newsapi.org/v2/everything?qInTitle=" + team +
+    $(".dropdown-item").on("click", function() {
+        team = $(this).text();
+        teamDashed = team.split(/\s+/).join('-');
+        teamAbbr = $(this).attr("id");
+        console.log(team);
+        console.log(teamAbbr);
+        console.log(teamDashed);
+        $("#team-selection").text(team);
+    })
+
+    $("#team-submit").on("click", function () {
+        event.preventDefault();
+
+        console.log(team);
+        console.log(teamAbbr);
+
+        var newsUrl = "https://newsapi.org/v2/everything?qInTitle=" + teamDashed +
             "&language=en&sortBy=publishedAt&from=2019-10-15&apiKey=661826d0bdfe4381ace783308aa9216c";
 
-        var seatgeekUrl = "https://api.seatgeek.com/2/events?performers.slug=" + team + "&client_id=MTkwNTE3NjN8MTU3MTg1OTczOS4yNA";
+        var seatgeekUrl = "https://api.seatgeek.com/2/events?performers.slug=" + teamDashed + "&client_id=MTkwNTE3NjN8MTU3MTg1OTczOS4yNA";
 
-        var mySportsFeedsUrl = "https://api.mysportsfeeds.com/v1.2/pull/nfl/2019-regular/scoreboard.json?fordate=20191013&team=" + team;
+        var mySportsFeedsUrl = "https://api.mysportsfeeds.com/v1.2/pull/nfl/2019-regular/scoreboard.json?fordate=20191013&team=" + teamDashed;
         // var mySportsFeedsUrl = "https://api.mysportsfeeds.com/v1.2/pull/nfl/2019-regular/game_boxscore.json?gameid=20190905-GB-CHI"
 
         // NEWS AJAX CALL
